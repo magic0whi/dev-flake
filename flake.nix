5a739ccc69e0ed6ff770cb95d39df37ca3295a00
Proteus Qian <sudaku233@outlook.com>
2026-05-22 04:24:22 +0800

    ✨ feat(rust): add rust development shell
    - create `rust/default.nix` providing a Nix development shell for Rust
    - add `cargo` and `rustc` to `buildInputs`
    - add `pkg-config` and `webkitgtk_4_1` to `nativeBuildInputs`
    - register the new rust shell in `flake.nix` `devShells` outputs
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- flake.nix
+++ flake.nix
@@ -90,6 +90,7 @@
           })
         ];
       };
+      rust = import_shell ./rust;
     });
   };
 }
---
+++ rust/default.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}: (pkgs.mkShell.override {
+    # Override stdenv to change compiler:
+    # stdenv = pkgs.clangStdenv;
+  } {
+    name = "Rust";
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+      webkitgtk_4_1
+      # wrapGAppsHook4
+      # cargo-tauri
+      # rustc
+    ];
+    buildInputs = with pkgs; [
+      cargo
+      rustc
+      # librsvg
+      # gtk3
+      # pango # Fixes the missing pango error
+      # cairo
+      # gdk-pixbuf
+    ];
+    shellHook = ''
+      rustc --version && cargo --version
+    '';
+  })
